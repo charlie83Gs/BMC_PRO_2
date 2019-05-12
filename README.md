747aba2c896d541588424999f40e6dc7ada3cbf2
Carlos <carlosgomezsoza@gmail.com>
2019-05-12 15:05:38 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1>Molecular Pathway Comparer<h1>
+
+<h2>Instituto Tecnologico de Costa Rica</h2>
+
+<h2>Made by:</h2>
+
+<ul>
+	<li>
+		<p>Carlos Gomez Soza</p>
+	</li>
+	<li>
+		<p>Yelson ... ...</p>
+	</li>
+</ul>
+
+
+<h2>Tasks</h2>
---
+++ src/component/MebolicRouteComparer.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+
+export default class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: (this.state.count + 1)
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: (this.state.count - 1)
+    });
+  };
+
+  render () {
+    return (
+      <div>
+        <h1> Metabolic Route Comparer</h1>
+        
+
+      </div>
+    );
+  }
+}
